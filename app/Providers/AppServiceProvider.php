6acd46da565f9da54f05331a69c7d639239f0d41
MohamedS0bhy <mohamedsobhy2296@gmial.com>
2018-11-04 09:18:14 +0200

    edit app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(request()->server('SERVER_NAME') == 'laravel57.herokuapp.com')
+        {
+            URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 
